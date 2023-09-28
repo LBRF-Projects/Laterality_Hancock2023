d1cda5c9d44899e19396d0820268c5be5956ccc3
Austin Hurst <mynameisaustinhurst@gmail.com>
2023-09-28 20:26:12 -0300

    Bump trial counts to 80, add block break msg

--- ExpAssets/Config/MotorMapping_params.py
+++ ExpAssets/Config/MotorMapping_params.py
@@ -40,7 +40,7 @@ saccadic_motion_threshold = 0.15
 # Experiment Structure
 #########################################
 multi_session_project = False
-trials_per_block = 60
+trials_per_block = 80
 blocks_per_experiment = 2
 table_defaults = {}
 conditions = ['PP', 'MI']
--- experiment.py
+++ experiment.py
@@ -205,6 +205,14 @@ class MotorMapping(klibs.Experiment):
 
     def trial_prep(self):
 
+        # If halfway through training or test block, do block break
+        if not P.practicing and (P.trial_number - 1) == int(P.trials_per_block / 2):
+            self.show_demo_text(
+                ("Take a short break! Whenever you're ready, press any button to\n"
+                 "resume the task."),
+                stim_set=[], msg_y=int(0.45 * P.screen_c[1])
+            )
+
         # Generate trial factors
         self.target_angle = randrange(0, 360, 1)
         self.target_dist = randrange(self.target_dist_min, self.target_dist_max)
